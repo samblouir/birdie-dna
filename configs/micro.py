a460a658974fce10080050eeffe95a03efcbdcaa
Sam Blouir <scblouir+github@gmail.com>
2025-01-29 18:31:15 -0500

    Simplified micro config

--- configs/micro.py
+++ configs/micro.py
@@ -1,32 +1,28 @@
-# configs/bert.py
+# configs/micro.py
+import configs.mini as mini_cfg
 """
 =============================================================================
-BERT NORMAL CONFIGURATION FILE
+MICRO CONFIGURATION FILE
 =============================================================================
 """
 
 from configs.default import get_config as get_default_config
 
 def get_config():
-    """
-    Loads in the default config, thne applies these updates
+	"""
+	Loads in the default config, thne applies these updates
 
-    Returns:
-        dict: The modified 'mini' configuration.
-    """
-    config = get_default_config()
-    config.update({
-        "config_name": "micro",
-
-        "batch_size": 16,
-        "sequence_length": 1024,
-        
-        # Taken literally for normalization: Each (Attn -> MLP) sequences counts as two layers
-        "num_layers": 4,
-        "num_steps": 32768,
-        "hidden_size": 512,
-        "num_heads": 8,
-        "head_dim": 64,
-        "eval_interval": 512,
-    })
-    return config
+	Returns:
+		dict: The modified 'micro' configuration.
+	"""
+	config = get_default_config()
+	
+	config.update({
+		**mini_cfg.get_config(),
+		"config_name": "micro",
+		"num_layers": 4,
+		"hidden_size": 512,
+		"num_heads": 8,
+		"head_dim": 64,
+	})
+	return config
